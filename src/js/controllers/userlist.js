2ffed465f92e66fb2528cab29e4ecf8538129f69
Hossein bonyadi <hosseinbonyadi2@gmail.com>
2025-01-18 18:07:11 +0330

    making request to show users

--- src/js/controllers/userlist.js
+++ src/js/controllers/userlist.js
@@ -1,2 +1,27 @@
-myApp.controller("userlistController", ['$scope', '$location', function($scope, $location) {
-}]);
+myApp.controller("userlistController", ['$scope', '$location', '$http', 'checkCookie', function($scope, $location, $http, checkCookie) {
+    const API_URL = "/api/manage/users";
+    let token = checkCookie.checkTokenCookie();
+    $scope.users = [];
+  
+    if (token) {
+      $scope.getData = function() {
+        $http({
+          method: 'GET',
+          url: API_URL,
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        }).then(function(response) {
+          $scope.users = response.data.users; // Assuming response.data.users is an array of user objects
+          console.log($scope.users); // Debugging
+        }, function(error) {
+          console.error("Error fetching user list:", error);
+          $scope.error = "Failed to load user list. Please try again.";
+        });
+      };
+  
+      $scope.getData();
+    } else {
+      $location.path('/login');
+    }
+  }]);
