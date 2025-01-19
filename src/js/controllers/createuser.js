bbb5abec922fb400c8e4db575d3b16ec8e1a84a8
Hossein bonyadi <hosseinbonyadi2@gmail.com>
2025-01-19 16:15:49 +0330

    createUser component and make form validation

--- src/js/controllers/createuser.js
+++ src/js/controllers/createuser.js
@@ -1,2 +1,12 @@
 myApp.controller("createuserController", ['$scope', '$location', function($scope, $location) {
+    $scope.user = {name:'',mobile :'',email:'',pass:"",repeatpass:''}
+    $scope.error={
+        name:'لطفا نام و نام خانوادگی را بدرستی درج نمایید',
+        mobile :'لطفا شماره موبایل را وارد نمایید',
+        email:'لطفا ایمیل را درج نمایید',
+        pass:"لطفا رمز عبور را بنویسید",
+        repeatpass:"لطفا  تکرار رمز عبور را بنویسید",
+        checkpass:"رمز عبور و تکرار رمز عبور باید با هم یکسان باشند",
+        passPattern:"/^(?=.*[a-zA-Z]).{6,}$/"
+    }
 }]);
